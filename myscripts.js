3ee4f410fcd7f12a7fd99aedde7882e29a930532
mkreynolds919 <mkreynolds919@gmail.com>
2024-04-18 20:40:55 -0500

    Add dimension button and javascript

--- myscripts.js
+++ myscripts.js
@@ -1,25 +1,44 @@
 
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (var x = 1; x <= 16; x++) {
-    const row = document.createElement("div");
-    row.id = "r"+x;
-    row.style.display = "flex";
-    row.style.flex = "1";
-    for (var i = 1; i <= 16; i++) {
-        const cell = document.createElement("div");
-        cell.style.flex = "1";
-        cell.id = `${row.id}c`+i;
-        cell.classList.toggle("cell");
-        row.appendChild(cell);
+button.addEventListener("click", () => {
+    let dimension = prompt("How many squares per side would you like?");
+    deleteGrid();
+    createGrid(dimension);
+})
+
+function createGrid(dimension = 16) { 
+    for (var x = 1; x <= dimension; x++) {
+        const row = document.createElement("div");
+        row.id = "r"+x;
+        row.classList.toggle("row");
+        row.style.display = "flex";
+        row.style.flex = "1";
+        for (var i = 1; i <= dimension; i++) {
+            const cell = document.createElement("div");
+            cell.style.flex = "1";
+            cell.id = `${row.id}c`+i;
+            cell.classList.toggle("cell");
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "blue";
+        });
+    });
 }
 
-const cells = document.querySelectorAll(".cell");
-
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "blue";
+function deleteGrid() {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        row.remove();
     })
-})
+}
+
+createGrid();
